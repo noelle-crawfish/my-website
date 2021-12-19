bbac3a4d8284e48d7c94f6b8220908cf3e0d407b
Noelle <noellecrawfish@gmail.com>
2021-12-19 13:56:21 -0600

    first post and other fixes

--- src/component/blog-list.js
+++ src/component/blog-list.js
@@ -6,12 +6,28 @@ import blogData from '../resources/blogs.json'
 import BlogItem from './blog-item.js'
 
 export default class BlogList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      blogContent: []
+    };
+  }
+
+  async componentWillMount() {
+    this.setState({
+      blogContent: await Promise.all(blogData.map((post) => fetch(post.contentUrl).then(response => response.text())))
+    });
+
+    //let ex = await fetch('resources/blogs/hello-world').then(response => response.text());
+    //console.log("ex " + ex);
+
+  }
+
   render() {
     return (
       <div className='blog-list'>
-        {blogData.map((post) => (
-          // get html from file
-          <BlogItem title={post.title} body={"temporary"}/>
+        {blogData.map((post, index) => (
+          <BlogItem title={post.title} date={post.date} body={this.state.blogContent[index]} />
         ))}
       </div>
     )
--- src/component/feature.js
+++ src/component/feature.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react'
 
 import styles from '../resources/styles.css'
-import BlogItem from './blog-item.js'
+import blogData from '../resources/blogs.json'
 
+import BlogItem from './blog-item.js'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default class Feature extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      content: ''
+    };
+  }
+
+  async componentWillMount() {
+    console.log('hi');
+    this.setState({
+       //await Promise.all(blogData.map((post) => fetch(post.contentUrl).then(response => response.text())))
+      content: await fetch(blogData[0].contentUrl).then(response => response.text())
+    });
+  }
+
   render() {
     return (
-      <div className='feature'>
-        <BlogItem />
+      <div className='featured-posts'>
+        <BlogItem title={ blogData[0].title } body={ this.state.content }/>
       </div>
     )
   }
